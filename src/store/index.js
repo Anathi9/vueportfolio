5786f77f7f14499fecdfcba7963bbc25e7538578
Anathi9 <anathijosefu@gmail.com>
2024-01-09 14:11:02 +0200

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,38 @@
 import { createStore } from 'vuex'
-
+const dataUrl = 'https://anathi9.github.io/portfolio_Data/'
 export default createStore({
   state: {
+    about: null,
+    education: null,
+    skills: null,
+    testimonials: null,
+    projects: null,
   },
   getters: {
   },
   mutations: {
+    setAbout(state, value) {
+      state.about = value
+    },
+    setEducation(state, value) {
+      state.education = value
+    },
+    setSkills(state, value) {
+      state.skills = value
+    },
+    setTestimonials(state, value) {
+      state.testimonials = value
+    },
+    setProjects(state, value) {
+      state.projects = value
+    }
   },
   actions: {
+    async fetchAbout(context){
+      let res = await fetch(dataUrl)
+      let {about} = await res.json()
+      context.commit('setAbout', about)
+    }
   },
   modules: {
   }
