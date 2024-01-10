2c219e297126d7eb1a79e41eb2aff5fce7ec3998
Anathi9 <anathijosefu@gmail.com>
2024-01-10 15:24:04 +0200

    updates

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,14 +3,15 @@
        <div class="row vh-100 ">
 <h2 >Projects</h2>
        </div>
-
     </div>
+    
 </template>
 
 <script>
     export default {
-        
-    }
+  
+     }  
+    
 </script>
 
 <style scoped>
