fe00e6f16e92ed3457a3106d46a35a90ba86cd34
Anathi9 <anathijosefu@gmail.com>
2024-01-10 09:50:45 +0200

    updates

---
+++ src/components/Spinner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+<spinner v-if="loading"/>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: 'SpinnerComp',
+       data() {
+        return{
+            loading:true,
+        };
+       },
+    };
+</script>
+
+<style scoped>
+
+</style>
